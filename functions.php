11139774fe24c0120b35ae6c5e263903bcce2838
scott kao <scott_kao@apple.com>
2017-08-16 20:39:21 -0700

    feat: initial dev set up

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 function my_theme_enqueue_styles() {
 
-    $parent_style = 'twentyfifteen-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+    $parent_style = 'shoreditch-style';
 
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style',
