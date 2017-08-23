9014c1d8a4abf8aa2f52681d9b789b9928eb4f04
Megan Prior-Pfeifer <mprior@apple.com>
2017-08-22 21:21:56 -0700

    feat: added basic gulp file sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
+'use strict';
+
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
 gulp.task('default', function() {
-  // place code for your default task here
+	return gulp.src('./src/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./'));
 });
