ba1b4e913e2eb5b19456685d1e029e84818d4439
scott kao <scottkao85@gmail.com>
2017-08-28 20:22:09 -0700

    feat: header styles

--- header.php
+++ header.php
@@ -62,12 +62,7 @@
 			<?php endif; ?>
 		</div>
 	</header><!-- #masthead -->
-
 	<div id="content" class="site-content">
 		<?php if ( get_header_image() ) : ?>
-			<div class="header-image">
-				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-					<img src="<?php header_image(); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="">
-				</a>
-			</div><!-- .header-image -->
+			
 		<?php endif; // End header image check. ?>
