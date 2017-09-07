f2db9f47d0a2fe8fc72153c63b60093913d10c0c
scott kao <scott_kao@apple.com>
2017-09-06 18:56:03 -0700

    bugfix: issue #31 included ref to font face

--- header.php
+++ header.php
@@ -16,6 +16,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link href="https://fonts.googleapis.com/css?family=Droid+Serif:400,700|Lato:400,700" rel="stylesheet">
 
 <?php wp_head(); ?>
 </head>
