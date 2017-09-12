87ced8f8b1224b613ce4f753801ba465c1a007be
mpriorpfeifer <mprior@unillu.com>
2017-09-11 22:17:31 -0700

    fix: header small nav bug issues. #52

--- header.php
+++ header.php
@@ -50,7 +50,7 @@
 
 			<?php if ( has_nav_menu( 'primary' ) ) : ?>
 				<button id="menu-toggle" class="menu-toggle"><?php esc_html_e( 'Menu', 'shoreditch' ); ?></button>
-				<div id="site-menu" class="site-menu color-primary dark-trans">
+				<div id="site-menu" class="site-menu">
 					<nav id="site-navigation" class="main-navigation" role="navigation" aria-label="<?php esc_html_e( 'Primary Menu', 'shoreditch' ); ?>">
 						<?php
 						wp_nav_menu( array(
