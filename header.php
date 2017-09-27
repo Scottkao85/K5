33f51d98b5a98b652786a86905b667889df316cf
mpriorpfeifer <mprior@unillu.com>
2017-09-26 19:39:13 -0700

    feat: ie specific css

--- header.php
+++ header.php
@@ -17,7 +17,9 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <link href="https://fonts.googleapis.com/css?family=Droid+Serif:400,700|Lato:400,700" rel="stylesheet">
-
+<!--[if IE]>
+	<link rel="stylesheet" type="text/css" href="style-ie.css" />
+<![endif]-->
 <?php wp_head(); ?>
 </head>
 
