6d8eaddaf89fbcb669c822b5b8faa5e0be0a48f0
mpriorpfeifer <mprior@unillu.com>
2017-09-26 18:42:27 -0700

    feat: added customization to the backend

--- functions.php
+++ functions.php
@@ -11,3 +11,17 @@ function my_theme_enqueue_styles() {
     );
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+function my_login_logo() { ?>
+    <style type="text/css">
+        #login h1 a, .login h1 a {
+            background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/site-login-logo.png);
+			height:83px;
+			width:83px;
+			background-size: 83px 83px;
+			background-repeat: no-repeat;
+	        padding-bottom: 0;
+        }
+    </style>
+<?php }
+add_action( 'login_enqueue_scripts', 'my_login_logo' );
